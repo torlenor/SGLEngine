891ae404960b057c858364d2b0a3a6c8a2c3b059
Hans-Peter Schadler <hps@abyle.org>
2014-08-30 22:30:34 +0200

    Updated compile.sh

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@ CPPFILES="sglengine.cpp sglengine_objparser.cpp test.cpp"
 if [ `uname` == "Linux" ] ; then
   g++ -o test.x $CPPFILES -lGLEW -lglfw3  -std=c++11 -lXxf86vm -lpthread -lXi -lXrandr -lGL -lX11 -Wall
 else 
-  g++ -o test.x $CPPFILES -lglew -lglfw3 -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -O2
+  g++ -o test.x $CPPFILES -lglew -lglfw3 -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -O2 -std=c++11
 fi
